d9ac62a311f43e99772f08186ac694b198098e6c
wilsonjojr <103847741+wilsonjojr@users.noreply.github.com>
2022-06-15 14:25:14 -0300

    Add files via upload
    Tela de inicio e de configuração.

---
+++ configu/configu.js
@@ -0,0 +1,3 @@
+function back1(){
+    window.location.href='../inicio.html';
+  }
---
+++ inicio.js
@@ -0,0 +1,7 @@
+function confi(){
+    window.location.href='configu/configu.html';
+  }
+
+  function play(){
+    window.location.href='dificuldade/dificuldade.html';
+  }
