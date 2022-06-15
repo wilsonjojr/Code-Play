255d8c32ce49ecffd8f3ac7954c7da04e90ad449
wilsonjojr <103847741+wilsonjojr@users.noreply.github.com>
2022-06-15 14:34:48 -0300

    Add files via upload

---
+++ dificuldade.js
@@ -0,0 +1,16 @@
+
+function easy(){
+    window.location.href='easy/easy.html';
+  }
+
+  function medium(){
+    window.location.href='medium/medium.html';
+  }
+
+  function hard(){
+    window.location.href='hard/hard.html';
+  }
+
+function back5(){
+    window.location.href='../inicio.html';
+  }
