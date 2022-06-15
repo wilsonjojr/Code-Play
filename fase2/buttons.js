9d8b7a6172c3c5a3d228f09ce302cf150a5e3ae7
wilsonjojr <103847741+wilsonjojr@users.noreply.github.com>
2022-06-15 14:32:39 -0300

    Add files via upload

--- fase2/buttons.js
+++ fase2/buttons.js
@@ -2,39 +2,39 @@
 
 /* A barra de navegação ocupa 25% da tela */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "25%";
-    document.body.style.backgroundColor = "#451C99";
-    /*document.getElementById("myModalDica").style.backgroundColor = "#451C99";*/
-  }
-  
-  /* A barra de navegação ocupa 0% da tela */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0%";
-    document.body.style.backgroundColor = "#5522BD";
-  }
-  
-  function confi(){
-    document.getElementById("conteudoModalConfig").style.display = "block";
-    document.body.style.backgroundColor = "#451C99";
-    document.getElementById("menuE").style.display = "none";
-    document.getElementById("resetG").style.display = "none";
-    document.getElementById("confirm").style.display = "none";
-    document.getElementById("myBtnDica").style.display = "none";
-    document.getElementById("mySidenav").style.width = "0%";
-  }
+  document.getElementById("mySidenav").style.width = "25%";
+  document.body.style.backgroundColor = "#451C99";
+  /*document.getElementById("myModalDica").style.backgroundColor = "#451C99";*/
+}
 
-  function closeConfi(){
-    document.getElementById("conteudoModalConfig").style.display = "none";
-    document.body.style.backgroundColor = "#5522BD";
-    document.getElementById("menuE").style.display = "block";
-    document.getElementById("resetG").style.display = "block";
-    document.getElementById("confirm").style.display = "block";
-    document.getElementById("myBtnDica").style.display = "block";
-  }
+/* A barra de navegação ocupa 0% da tela */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0%";
+  document.body.style.backgroundColor = "#5522BD";
+}
 
-  function exit(){
-    window.location.href='../hard.html';
-  }
+function confi(){
+  document.getElementById("conteudoModalConfig").style.display = "block";
+  document.body.style.backgroundColor = "#451C99";
+  document.getElementById("menuE").style.display = "none";
+  document.getElementById("resetG").style.display = "none";
+  document.getElementById("confirm").style.display = "none";
+  document.getElementById("myBtnDica").style.display = "none";
+  document.getElementById("mySidenav").style.width = "0%";
+}
+
+function closeConfi(){
+  document.getElementById("conteudoModalConfig").style.display = "none";
+  document.body.style.backgroundColor = "#5522BD";
+  document.getElementById("menuE").style.display = "block";
+  document.getElementById("resetG").style.display = "block";
+  document.getElementById("confirm").style.display = "block";
+  document.getElementById("myBtnDica").style.display = "block";
+}
+
+function exit(){
+  window.location.href='../medium.html';
+}
 
 
 
@@ -53,65 +53,66 @@ var spanDica = document.getElementsByClassName("closeDica")[0];
 
 // Quando o usuário clicar no botão, abra o modal 
 btnDica.onclick = function() {
-  modalDica.style.display = "block";
+modalDica.style.display = "block";
 }
 
 // Quando o usuário clicar no X, feche o modal
 spanDica.onclick = function() {
-  modalDica.style.display = "none";
+modalDica.style.display = "none";
 }
 
 //Quando o usuário clicar fora do modal, feche o modal
 window.onclick = function(event) {
-  if (event.target == modalDica) {
-    modalDica.style.display = "none";
-  }
+if (event.target == modalDica) {
+  modalDica.style.display = "none";
+}
 
-  
+
+//Tirar os elementos da tela quando a tela de vitoria ou derrota aparecer
+
 }
 
 document.getElementById("demo").innerHTML = 0;
 
 function randomN(){
-  document.getElementById("demo").innerHTML =
+document.getElementById("demo").innerHTML =
 Math.floor(Math.random() * 10);
 
 if(document.getElementById("demo").innerHTML <=5){
-  document.getElementById("conteudoModalDefeat").style.display = "block";
-  document.body.style.color = "#451C99";
-  document.getElementById("menuE").style.display = "none";
-  document.getElementById("resetG").style.display = "none";
-  document.getElementById("confirm").style.display = "none";
-  document.getElementById("myBtnDica").style.display = "none";
- 
-  
+document.getElementById("conteudoModalDefeat").style.display = "block";
+document.body.style.color = "#451C99";
+document.getElementById("menuE").style.display = "none";
+document.getElementById("resetG").style.display = "none";
+document.getElementById("confirm").style.display = "none";
+document.getElementById("myBtnDica").style.display = "none";
+
+
 }
 
 if(document.getElementById("demo").innerHTML >5){
-  document.getElementById("conteudoModalVitory").style.display = "block";
-  document.body.style.color = "#451C99";
-  document.getElementById("menuE").style.display = "none";
-  document.getElementById("resetG").style.display = "none";
-  document.getElementById("confirm").style.display = "none";
-  document.getElementById("myBtnDica").style.display = "none";
-  
+document.getElementById("conteudoModalVitory").style.display = "block";
+document.body.style.color = "#451C99";
+document.getElementById("menuE").style.display = "none";
+document.getElementById("resetG").style.display = "none";
+document.getElementById("confirm").style.display = "none";
+document.getElementById("myBtnDica").style.display = "none";
+
 }
 }
 
 function back1(){
-  window.location.href='../../../inicio.html';
+window.location.href='../../../inicio.html';
 }
 
 function reset(){
-  window.location.href='fase2.html';
+window.location.href='fase2.html';
 }
 
 function next(){
-  window.location.href='../fase3/fase3.html';
+window.location.href='../fase3/fase3.html';
 }
 
 
-  
 
 
-  
+
---
+++ hard.js
@@ -0,0 +1,35 @@
+function fase1(){
+    window.location.href='fase1/fase1.html';
+  }
+  
+  function fase2(){
+    window.location.href='fase2/fase2.html';
+  }
+  
+  function fase3(){
+    window.location.href='fase3/fase3.html';
+  }
+  
+  function fase4(){
+    window.location.href='fase4/fase4.html';
+  }
+  
+  function fase5(){
+    window.location.href='fase5/fase5.html';
+  }
+  
+  function fase6(){
+    window.location.href='fase6/fase6.html';
+  }
+  
+  function fase7(){
+    window.location.href='fase7/fase7.html';
+  }
+  
+  function fase8(){
+    window.location.href='fase8/fase8.html';
+  }
+  
+  function back5(){
+      window.location.href='../dificuldade.html';
+    }
